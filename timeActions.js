adc3435a8b7c58229d6282f0d4959f70a9958a73
b2ock <118326205+b2ock@users.noreply.github.com>
2024-09-27 12:15:06 -0600

    trying js to lock screen portrait

--- timeActions.js
+++ timeActions.js
@@ -3,3 +3,9 @@ let dayOfTheWeek = new Date().getDay();
 daySelector.selectedIndex = dayOfTheWeek - 1;
 
 getDayWorkouts();
+
+if (screen.orientation) {
+    screen.orientation.lock('portraint').catch(function(error) {
+        console.error("Screen orientation lock failed: ", error);
+    })
+}
