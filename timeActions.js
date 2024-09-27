387d42bcd924a4f79cabdd3a872472d84884f9fb
b2ock <118326205+b2ock@users.noreply.github.com>
2024-09-27 12:17:18 -0600

    Trying js again for orientation lock

--- timeActions.js
+++ timeActions.js
@@ -5,7 +5,7 @@ daySelector.selectedIndex = dayOfTheWeek - 1;
 getDayWorkouts();
 
 if (screen.orientation) {
-    screen.orientation.lock('portraint').catch(function(error) {
+    screen.orientation.lock('portrait').catch(function(error) {
         console.error("Screen orientation lock failed: ", error);
     })
 }
