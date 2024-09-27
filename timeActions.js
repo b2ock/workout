56a455722f1caa696538d6ac115a50cfd45e6751
b2ock <118326205+b2ock@users.noreply.github.com>
2024-09-27 12:19:49 -0600

    js did not work removing changes will try again later

--- timeActions.js
+++ timeActions.js
@@ -3,9 +3,3 @@ let dayOfTheWeek = new Date().getDay();
 daySelector.selectedIndex = dayOfTheWeek - 1;
 
 getDayWorkouts();
-
-if (screen.orientation) {
-    screen.orientation.lock('portrait').catch(function(error) {
-        console.error("Screen orientation lock failed: ", error);
-    })
-}
